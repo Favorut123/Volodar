6494e9105669d3bc2cda1ea5a42ecd4de730817d
Favorut123 <volodarchuhay@gmail.com>
2020-03-10 15:48:43 +0200

    hw

---
+++ Vololdar/HomeWork/lesson4/1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace homework4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                if (i == 5)
+                {
+                    continue;
+                }      
+                
+                Console.WriteLine(i);
+
+
+            }
+
+        }
+
+    }
+}
---
+++ Vololdar/HomeWork/lesson4/2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace hw5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 39; i++)
+            {
+                if ((i % 2) == 1)
+                    continue;
+                Console.WriteLine(i);
+            }
+
+
+        }
+
+    }
+}
